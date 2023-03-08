2b7df5ac73f36352443dfa44f6b13b15353900ff
petboa <112291489+petboa@users.noreply.github.com>
2023-03-08 17:50:45 +0000

    py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
+    if i < 10:
+        i = "0" + str(i)
     if i == 99:
-        print(i)
-else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+        print("{}".format(i))
+        break
+    print("{}".format(i), end=", ")
