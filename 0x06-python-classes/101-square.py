da7a1bdc1861b124beb5721c9fe2a70d9f758a6e
petboa <112291489+petboa@users.noreply.github.com>
2023-03-28 18:38:20 +0000

    py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,70 +1,78 @@
 #!/usr/bin/python3
-
-]11;rgb:0000/0000/0000\""" Write a class Square that defines a square by: (based on 1-square.py)."""
+"""Square module."""
 
 
 class Square:
-    """Represent a square."""
+    """Defines a square."""
+
+    def __str__(self):
+        """Returns string representation."""
+        return self.my_sprint()[:-1]
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new Square.
+        """Constructor.
         Args:
-            size (integer): The size of the new square.
-            position(integer, integer): The position of the new square
+            size: Length of a side of the square.
+            position: Position of the square.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/Set the current size of the square."""
-        return (self.__size)
+        """Property for the length of a side of this square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     @property
     def position(self):
-        """Retrieve the current size of the square."""
-        return (self.__position)
+        """Property for the position of this square.
+        Raises:
+            TypeError: If value is not tuple of 2 positive integers.
+        """
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value, tuple) or len(value) != 2 or \
+         len([x for x in value if isinstance(x, int) and x >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
     def area(self):
-        """Returns the current square area"""
+        """Area of this square.
+        Returns:
+            The size squared.
+        """
+        return self.__size ** 2
 
-        return (self.__size * self.__size)
+    def my_sprint(self):
+        """Returns string representation of this square."""
+        ret = ""
+        if not self.size:
+            return "\n"
+
+        for i in range(self.position[1]):
+                ret += "\n"
+        for i in range(self.size):
+            for j in range(self.position[0]):
+                ret += " "
+            for j in range(self.size):
+                ret += "#"
+            ret += "\n"
+        return ret
 
     def my_print(self):
-        """Print the square with the # character"""
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
-
-    def __str__(self):
-        """Define the print() representation of a Square."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+        """Prints this square."""
+        print(self.my_sprint(), end="")
