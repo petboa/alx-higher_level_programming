6c10be3f2be5c360db99a6f3e76c487b9ab1cfc2
petboa123@gmail.com <petboa123@gmail.com>
2023-04-06 04:23:46 -0700

    py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,17 +4,17 @@
 
 class Rectangle:
     """Defines a rectangle"""
-    
+
     def __init__(self, width=0, height=0):
         """Constructor"""
-        
+
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -23,11 +23,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
