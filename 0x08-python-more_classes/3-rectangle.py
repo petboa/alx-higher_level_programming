4cb2c42c45e678ec7fa0ddbb1a4c3e96f657aa90
petboa <112291489+petboa@users.noreply.github.com>
2023-04-14 17:50:32 +0000

    py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """rectangle class"""
 
-
 class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
@@ -13,7 +12,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -26,7 +25,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -37,16 +36,10 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
-            return 0
-        else:
-            return 2 * (self.width + self.height)
+        return 2 * (self.width + self.height) if self.width and self.height else 0
 
     def __str__(self):
         if self.width == 0 or self.height == 0:
-            return ""
+            return ''
         else:
-            rect = ""
-            for i in range(self.height):
-                rect += "#" * self.width + "\n"
-            return rect[:-1]
+            return '\n'.join(['#' * self.width] * self.height)
