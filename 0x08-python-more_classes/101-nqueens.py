928c83c6de7a97bb8622c8870de56847c0b13652
petboa <112291489+petboa@users.noreply.github.com>
2023-04-23 16:31:43 +0000

    py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,31 +2,30 @@
 
 import sys
 
-
-def print_board(board):
+def print_solution(board):
     if any(1 in x for x in board):
         print([[idx, board[idx].index(1)] for idx, val in enumerate(board)])
 
 
-def is_safe(row, square, chessboard, Nm, diags):
-    if chessboard[row][square]:
+def is_square_safe(row, col, board, n, diags):
+    if board[row][col]:
         return False
-    if square - diags >= 0 and chessboard[row][square - diags]:
+    if col - diags >= 0 and board[row][col - diags]:
         return False
-    if square + diags < Nm and chessboard[row][square + diags]:
+    if col + diags < (n) and board[row][col + diags]:
         return False
     if row == 0:
         return True
-    return is_safe(row - 1, square, chessboard, Nm, diags + 1)
+    return is_square_safe(row - 1, col, board, n, diags + 1)
 
 
-def place_square(row, position, chessboard, Nm):
-    for square in range(position, Nm):
-        if 1 in chessboard[row]:
+def place_queen(row, col, board, n):
+    for c in range(col, n):
+        if 1 in board[row]:
             return 0
-        if not is_safe(row - 1, square, chessboard, Nm, 1):
+        if not is_square_safe(row - 1, c, board, n, 1):
             continue
-        chessboard[row][square] = 1
+        board[row][c] = 1
         return
     return 1
 
@@ -35,35 +34,35 @@ if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
 
-N = sys.argv[1]
+n = sys.argv[1]
 
-if not N.isdigit():
+if not str.isdigit(n):
     print("N must be a number")
     sys.exit(1)
 
-N = int(N)
+n = int(n)
 
-if N < 4:
+if n < 4:
     print("N must be at least 4")
     sys.exit(1)
 
 queen = 0
 
-while queen != N:
-    chessboard = [[0 for x in range(N)] for x in range(N)]
-    chessboard[0][queen] = 1
-    position = 0
+while queen != n:
+    board = [[0 for x in range(n)] for x in range(n)]
+    board[0][queen] = 1
+    col = 0
     row = 1
-    while row < N:
-        if place_square(row, position, chessboard, N):
+    while row < n:
+        if place_queen(row, col, board, n):
             row -= 1
-            position = chessboard[row].index(1)
-            chessboard[row][position] = 0
-            position += 1
+            col = board[row].index(1)
+            board[row][col] = 0
+            col += 1
             if not row:
                 break
         else:
             row += 1
-            position = 0
-    print_board(chessboard)
+            col = 0
+    print_solution(board)
     queen += 1
