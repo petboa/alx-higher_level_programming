43425e80efe2100281556a5acaf677237771d178
petboa <112291489+petboa@users.noreply.github.com>
2023-04-26 12:23:55 +0000

    py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,13 +3,17 @@
     add_integer(a, b=98)
 """
 
-def add_integer(a, b=98):
+
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
     """
-    This function takes two arguments, a and b, and returns their sum.
-    If either argument is not an integer or a float, a TypeError is raised.
-    """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
