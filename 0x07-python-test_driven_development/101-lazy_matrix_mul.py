99ba7abea397f00ef4527f1fa0fba1b048d5889b
petboa <112291489+petboa@users.noreply.github.com>
2023-04-26 12:48:27 +0000

    py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""A function that performs matrix multiplication using NumPy."""
+"""Defines a matrix multiplication function."""
 
-import numpy as np
-
-
-def multiply_matrices(m_a, m_b):
-    """Multiply two matrices using NumPy.
-    
-    Arguments:
-    m_a -- The first matrix.
-    m_b -- The second matrix.
-    
-    Returns:
-    The product of the two matrices.
+def matrix_multiply(m_a, m_b):
+    """Return the multiplication of two matrices.
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     """
-    return np.matmul(m_a, m_b)
+    rows_a = len(m_a)
+    cols_a = len(m_a[0])
+    rows_b = len(m_b)
+    cols_b = len(m_b[0])
+    if cols_a != rows_b:
+        return None
+    result = [[0 for row in range(cols_b)] for col in range(rows_a)]
+    for i in range(rows_a):
+        for j in range(cols_b):
+            for k in range(cols_a):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+    return result
