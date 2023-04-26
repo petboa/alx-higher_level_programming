528636b715a52ce8c0909caa269218b76356d587
petboa123@gmail.com <petboa123@gmail.com>
2023-04-26 05:29:26 -0700

    py

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_string - Prints information about a Python string object
+ * @p: Pointer to a PyObject
+ */
+void print_python_string(PyObject *p)
+{
+    Py_ssize_t size;
+    Py_UCS4 *str;
+
+    printf("[.] string object info\n");
+
+    if (!PyUnicode_Check(p)) {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+
+    size = PyUnicode_GetLength(p);
+    str = PyUnicode_AsUCS4(p);
+
+    printf("  type: %s\n", Py_TYPE(p)->tp_name);
+    printf("  length: %ld\n", size);
+    printf("  value: %ls\n", str);
+
+    /* Do not forget to free the memory allocated by PyUnicode_AsUCS4() */
+    PyMem_Free(str);
+}
