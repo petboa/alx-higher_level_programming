ccbcb2c9ee4a18ddeb736fa30b2e649e27fdf8d3
petboa <112291489+petboa@users.noreply.github.com>
2023-04-26 12:52:45 +0000

    py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,16 +2,22 @@
 """Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
-
-def matrix_multiply(m_a, m_b):
-    """Return the result of multiplying two matrices.
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiplies two matrices using NumPy.
 
     Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
+        m_a (list of lists): The first matrix.
+        m_b (list of lists): The second matrix.
 
     Returns:
         A new matrix resulting from the multiplication of m_a and m_b.
     """
+    try:
+        result = np.matmul(m_a, m_b)
+    except ValueError as error:
+        raise ValueError("Matrices cannot be multiplied: {}".format(error))
+    except Exception as error:
+        raise Exception("An error occurred while multiplying matrices: {}".format(error))
+    return result
 
-    return (np.matmul(m_a, m_b))
