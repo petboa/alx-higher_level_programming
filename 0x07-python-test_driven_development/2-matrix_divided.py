71dbc0d4907f825c84d5740e166858514bdae2d6
petboa <112291489+petboa@users.noreply.github.com>
2023-04-29 21:45:51 +0000

    py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,33 +5,27 @@
 def matrix_divided(matrix_list, divider):
     """Divides all elements of a matrix by divider.
     Args:
-        matrix_list: list of lists containing int/ floats.
+        matrix_list: list of lists containing integers/floats.
         divider: number to divide matrix by.
     Returns:
         List: list of lists containing divided matrix.
     Raises:
-        TypeError: if matrix_list is not list of lists nor contains ints/ floats.
+        TypeError: if matrix_list is not a list of lists or does not contain integers/floats.
         TypeError: if sublists are not of the same size.
-        TypeError: if divider is neither int nor float.
+        TypeError: if divider is not an int or float.
         ZeroDivisionError: when divider is zero.
     """
     if not isinstance(divider, (int, float)):
         raise TypeError("divider must be a number")
-    if not isinstance(matrix_list, list) or len(matrix_list) == 0:
-        raise TypeError("matrix_list must be a matrix (list of lists) of \
-                        integers/floats.")
+    if not isinstance(matrix_list, list) or len(matrix_list) == 0 or not all(isinstance(i, list) and all(isinstance(j, (int, float)) for j in i) for i in matrix_list):
+        raise TypeError("matrix_list must be a matrix (list of lists) of integers/floats.")
 
     for row in matrix_list:
-        if not isinstance(row, list) or len(row) == 0:
-            raise TypeError("matrix_list must be a matrix (list of lists) of \
-                            integers/floats.")
         if len(row) != len(matrix_list[0]):
             raise TypeError("Each row of the matrix must have the same size")
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError("matrix_list must be a matrix (list of lists) of \
-                    integers/floats.")
+            
     return [[round(x / divider, 2) for x in row] for row in matrix_list]
+        
 
 if __name__ == "__main__":
     import doctest
