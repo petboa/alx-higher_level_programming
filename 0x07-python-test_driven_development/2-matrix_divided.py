5db1e7f4670f7a780d7d2a54b9d530fc65bf75e9
petboa <112291489+petboa@users.noreply.github.com>
2023-04-29 22:14:38 +0000

    py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,14 +10,16 @@ def matrix_divided(matrix_list, divider):
     Returns:
         List: list of lists containing divided matrix.
     Raises:
-        TypeError: if matrix_list is not a list of lists or does not contain integers/floats.
+        TypeError: if matrix_list is not a
+        list of lists or does not contain integers/floats.
         TypeError: if sublists are not of the same size.
         TypeError: if divider is not an int or float.
         ZeroDivisionError: when divider is zero.
     """
     if not isinstance(divider, (int, float)):
         raise TypeError("divider must be a number")
-    if not isinstance(matrix_list, list) or len(matrix_list) == 0 or not all(isinstance(i, list) and all(isinstance(j, (int, float)) for j in i) for i in matrix_list):
+    if not isinstance(matrix_list, list) or len(matrix_list) == 0 or not 
+    all(isinstance(i, list) and all(isinstance(j, (int, float)) for j in i) for i in matrix_list):
         raise TypeError("matrix_list must be a matrix (list of lists) of integers/floats.")
 
     for row in matrix_list:
