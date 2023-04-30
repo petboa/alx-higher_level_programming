d3326b0d11bb4353de253e09dc5d3644b01762fd
petboa <112291489+petboa@users.noreply.github.com>
2023-04-30 21:04:37 +0000

    py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
+
 """
 This is the "2-matrix_divided" module.
 The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
 
-
 def matrix_divided(matrix, div):
-    """Divides all elements in the matrix by div"""
+    """
+    Divides all elements in the matrix by div
+    """
     if type(matrix) is not list:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     size = None
-    for l in matrix:
-        if type(l) is not list:
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         if size is None:
-            size = len(l)
-        elif size != len(l):
+            size = len(row)
+        elif size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
-        for i in l:
-            if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-    if type(div) is not int and type(div) is not float:
+        for element in row:
+            if type(element) not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for l in matrix]
+    return [[round(element / div, 2) for element in row] for row in matrix]
