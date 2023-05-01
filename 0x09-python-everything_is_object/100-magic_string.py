517563ab750b062cfdfdedb32507dc319f6b0142
petboa <112291489+petboa@users.noreply.github.com>
2023-05-01 07:07:14 +0000

    py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    s = "BestSchool"
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ", ".join([s] * magic_string.count)
+    return ", ".join([BestSchool] * magic_string.count)
