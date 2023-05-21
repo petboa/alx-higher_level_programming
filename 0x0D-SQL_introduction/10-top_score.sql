2e983857a0ceb58f776e76d737c64227124e9cdb
Peter Boakye <112291489+petboa@users.noreply.github.com>
2023-05-22 00:20:59 +0100

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
-    -- script that lists all records of the table second_table ordered by score
+-- script that lists all records of the table second_table ordered by score
 SELECT score, cname FROM second_table ORDER BY score DESC;
