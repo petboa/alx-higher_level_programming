7bceb10eb8592118e3f5f1f49f09f8840b634eca
Peter Boakye <petboa123@gmail.com>
2023-06-12 22:36:00 +0000

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
-let num = process.argv[2];
-
-if (!isNaN(num)) {
-  for (let i = 0; i < num; i++) {
-    console.log('C is fun');
-  }
-} else {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
