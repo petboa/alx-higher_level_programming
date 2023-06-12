d594b75c7bb0a0085f6d3e044feadfd9859ad383
Peter Boakye <petboa123@gmail.com>
2023-06-12 22:33:53 +0000

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-let num = process.argv[2];
-
-if (isNaN(num)) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < num; i++) {
-    let msg = '';
-    for (let j = 0; j < num; j++) {
-      msg = msg + 'X';
-    }
-    console.log(msg);
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
   }
 }
