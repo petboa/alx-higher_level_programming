8d5559b0a8aafc4b914f6386d0e965f323318cd7
Peter Boakye <petboa123@gmail.com>
2023-06-27 19:02:33 +0000

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+"""Find peak in list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
+    """Find peak in unsorted list
+    """
+    loi = list_of_integers
+    size = len(loi)
 
-    if list_of_integers is None or list_of_integers == []:
+    if size == 0:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    if size is 1:
+        return loi[0]
+
+    return recurse(loi, 0, size - 1)
+
+
+def recurse(loi, left, right):
+    """Recursive component
+    """
+    mid = int((left + right) / 2)
+
+    if left > right:
+        return loi[mid]
+
+    if (mid == 0 or loi[mid] > loi[mid - 1])\
+       and (mid == len(loi) - 1 or loi[mid] > loi[mid + 1]):
+        return loi[mid]
+
+    # recurse left
+    elif (mid > 0) and loi[mid - 1] > loi[mid]:
+        return recurse(loi, left, mid - 1)
+    else:  # recurse right
+        return recurse(loi, mid + 1, right)
