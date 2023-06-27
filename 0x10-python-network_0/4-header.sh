a46f5e5f2fbca6fc07cefd3a6ea7b8de4e7177ec
Peter Boakye <petboa123@gmail.com>
2023-06-27 19:17:58 +0000

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display response body
-curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl "$1" -sX GET -H "X-HolbertonSchool-User-Id:98"
