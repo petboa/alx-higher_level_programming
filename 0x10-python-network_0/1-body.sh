869974e16692d82d7708e35ca9fca9b8472a2774
Peter Boakye <petboa123@gmail.com>
2023-06-27 18:45:10 +0000

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-url="$1"
-curl -sL $url
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
