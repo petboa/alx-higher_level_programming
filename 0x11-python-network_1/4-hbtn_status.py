2f7deb87d211dbac9daa15a0292cb1ad69792d35
Peter Boakye <petboa123@gmail.com>
2023-06-30 18:08:32 +0000

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
-""" Python script that fetches https://intranet.hbtn.io/status """
+"""
+Same as Task 0 but using request package
+"""
 
 import requests
 
+
 if __name__ == "__main__":
     response = requests.get('https://intranet.hbtn.io/status')
-    data = response.text
     print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
