d65f97e3d2aa2af860c0487ee76dc8a954aa5c2d
Peter Boakye <petboa123@gmail.com>
2023-06-30 18:08:12 +0000

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+""" Takes in a URL and sends a request to the URL
+and displays the body of the response (decoded in utf-8)
+"""
 
-""" Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8). """
-
-from urllib import request, error
+import urllib.request
 import sys
 
 if __name__ == "__main__":
     try:
-        with request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-    
-    
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
+    except:
+        pass
