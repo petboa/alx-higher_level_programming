9107b7473332ad4dd8ae1c33790d767d2444f67b
Peter Boakye <petboa123@gmail.com>
2023-06-30 18:07:37 +0000

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""Lists 10 commits from the most recent to the oldest
+of the repository "rails" by the user rails
+"""
 
-""" Python script that lists 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails” """
-
-import requests
 import sys
+import requests
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1])
-    response = requests.get(url)
-    for commit in response.json()[:10]:
-        print("{}: {}".format(commit.get('sha'), commit.get('commit').get('author').get('name')))
+    repo_name = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo_name)
+
+    request = requests.get(url)
+    commits = request.json()
+    try:
+        for i in range(10):
+            print('{}: {}'.format(
+                commits[i].get('sha'),
+                commits[i].get('commit').get('author').get('name')))
+    except IndexError:
+        pass
