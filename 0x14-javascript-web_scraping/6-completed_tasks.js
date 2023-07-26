43d6371de77342741095cb767011aeb078a005c2
Peter Boakye <petboa123@gmail.com>
2023-07-26 13:13:10 +0000

    web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,35 @@
 #!/usr/bin/node
+// Script that computes the number of tasks completed by user id.
 
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
+
+request(url, function (err, response, body)
+{   
+    if (err)
+    {
+        console.log(err);
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
-});
+    else
+    {
+        let completedTasks = {};
+        let tasks = JSON.parse(body);
+        for (let i = 0; i < tasks.length; i++)
+        {
+            if (tasks[i].completed === true)
+            {
+                if (completedTasks[tasks[i].userId] === undefined)
+                {
+                    completedTasks[tasks[i].userId] = 1;
+                }
+                else
+                {
+                    completedTasks[tasks[i].userId] += 1;
+                }
+            }
+        }
+        console.log(completedTasks);
+    }
+}
+);
